76971bb5eeefe627fb77af587be6d526c317ddd3
amitgroch <amitgrochdennis@yahoo.com>
2017-04-16 09:52:47 +0530

    Angular application add new component movie and character components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { CharacterListComponent } from './components/character-list/character-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesListComponent,
+    CharacterListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+  RouterModule.forRoot([
+    {
+      path:'movies-list',
+      component:MoviesListComponent
+    },
+    {
+      path:'character-list',
+      component:CharacterListComponent
+    }
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
